5ae00b0c169d6cdabc20ed5fe18425163cf4d1d6
Amigo Reyes <Jerahmee@Amigo-Reyes.local>
2013-02-18 22:18:57 +0800

    hehehYeah

---
+++ IgoAndBear/IgoAndBear/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IgoAndBear
+//
+//  Created by Amigo Reyes on 2/18/13.
+//  Copyright (c) 2013 Amigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IgoAndBear/IgoAndBear/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IgoAndBear
+//
+//  Created by Amigo Reyes on 2/18/13.
+//  Copyright (c) 2013 Amigo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
